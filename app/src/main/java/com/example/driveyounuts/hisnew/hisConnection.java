6a1438585a8f46e48139dfd466fdbfa3059d43a6
denghui <297939484@qq.com>
2016-10-14 16:23:53 +0800

    修改回争取外网ip 测试完成

--- app/src/main/java/com/example/driveyounuts/hisnew/hisConnection.java
+++ app/src/main/java/com/example/driveyounuts/hisnew/hisConnection.java
@@ -25,8 +25,8 @@ public class hisConnection {
     
 
     
-    public static String url="jdbc:oracle:thin:@221.2.220.182/:1521:orcl";
-    //修改了外网的ip地址 181 为182 做github 测试使用
+    public static String url="jdbc:oracle:thin:@221.2.220.181/:1521:orcl";
+    //修改了外网的ip地址  便于手机直接测试使用
    
 
 
